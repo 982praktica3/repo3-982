edd352b1fbdb5f5d65b26179a74678efd4486b9d
Ksen-Chek <65395448+Ksen-Chek@users.noreply.github.com>
2020-05-16 16:28:51 +0500

    Update Задание 2.py

--- Задание 2.py
+++ Задание 2.py
@@ -1,4 +1,4 @@
 from random import randint
-n, m = int(input()), int(input()) 
+n, m = int(input('Введите размер массива: ')), int(input()) 
 a = [[randint(-100, 100) for j in range(m)] for i in range(n)]
 for i in range(n):print(a[i])
