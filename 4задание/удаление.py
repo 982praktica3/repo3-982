4cbdb5b024da0a494e5050a6d7e9a9af9213f1b0
vlad-rusalimov <65409742+vlad-rusalimov@users.noreply.github.com>
2020-05-25 00:44:43 +0500

    Delete удаление.py

--- 4задание/удаление.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-conn = sqlite3.connect("pr.db")
-cursor = conn.cursor()
-
-sql = input('Введите ID объекта для удаления: ')
-
-cursor.execute("DELETE FROM info WHERE id=?", sql)
-conn.commit()
