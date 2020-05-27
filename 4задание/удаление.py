93e2ee15c6674638fefa3ae51ce0b10b7e9f950d
Ksen-Chek <65395448+Ksen-Chek@users.noreply.github.com>
2020-05-28 02:23:21 +0500

    Update удаление.py

--- 4задание/удаление.py
+++ 4задание/удаление.py
@@ -1,9 +1,21 @@
 import sqlite3
 
-conn = sqlite3.connect("pr.db")
-cursor = conn.cursor()
-
+con = sqlite3.connect("pr.db")
+cursor = con.cursor()
 sql = input('Введите ID объекта для удаления: ')
 
 cursor.execute("DELETE FROM info WHERE id=?", sql)
-conn.commit()
+
+def sql_fetch(con):
+    a = 1
+    cursor = con.cursor()
+    cursor.execute('SELECT * FROM info')
+    rows = cursor.fetchall()
+    for row in rows:
+        s=str(a)
+        a=a+1
+        b=str(a)
+        cursor.execute("UPDATE info SET id=? WHERE id=?", (s, b)) 
+        
+sql_fetch(con)        
+con.commit()
