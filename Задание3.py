b9ba776d34fc72998c3a0984d39cdbb29e11d2cc
vlad-rusalimov <65409742+vlad-rusalimov@users.noreply.github.com>
2020-05-29 19:46:09 +0500

    Delete Задание3.py

--- Задание3.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-n = int(input('Введите размер массивов: '))
-a = np.random.randint(-50, 50, n)
-b = np.random.randint(-50, 50, n)
-c = sorted(np.concatenate((a, b)))
-print('Изначальные массивы: ', a, ';', b)
-print('Отсортированный массив', c)
-
