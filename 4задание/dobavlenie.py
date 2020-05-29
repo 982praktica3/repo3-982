ba7c6f603a2c38e5a3926732a1990fe7a07ee687
Ksen-Chek <65395448+Ksen-Chek@users.noreply.github.com>
2020-05-29 22:44:53 +0500

    Update dobavlenie.py

--- 4задание/dobavlenie.py
+++ 4задание/dobavlenie.py
@@ -1,16 +1,18 @@
 import sqlite3
-conn = sqlite3.connect("pr.db")
-cursor = conn.cursor()
+def dobav():
+ conn = sqlite3.connect("pr.db")
+ cursor = conn.cursor()
     # Вводим данные
-name = input('ФИО: ')
-date = input('Дата рождения: ')
-mobil = input('Номер мобильного: ')
-toils = input('Соц. сети: ')
-address = input('Адрес: ')
+ name = input('ФИО: ')
+ date = input('Дата рождения: ')
+ mobil = input('Номер мобильного: ')
+ toils = input('Соц. сети: ')
+ address = input('Адрес: ')
     # Вставляем данные в таблицу
-cursor.execute('''INSERT INTO info(name, date, mobil, toils, address) VALUES (?, ?, ?, ?, ?)''', (name, date, mobil, toils, address))
+ cursor.execute('''INSERT INTO info(name, date, mobil, toils, address) VALUES (?, ?, ?, ?, ?)''', (name, date, mobil, toils, address))
     # Сохраняем изменения
-conn.commit()
-cursor.close()
-conn.close()
-print('Объект добавлен')
+ conn.commit()
+ cursor.close()
+ conn.close()
+ print('Объект добавлен')
+ return
