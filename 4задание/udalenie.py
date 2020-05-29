dce2b904f98996de74032e9a38adafbe75e6065f
Ksen-Chek <65395448+Ksen-Chek@users.noreply.github.com>
2020-05-29 22:44:12 +0500

    Update udalenie.py

--- 4задание/udalenie.py
+++ 4задание/udalenie.py
@@ -1,9 +1,8 @@
 import sqlite3
-
-conn = sqlite3.connect("pr.db")
-cursor = conn.cursor()
-sql = input('Введите ID объекта для удаления: ')
-
-cursor.execute("DELETE FROM info WHERE id=?", sql)
-
-conn.commit()
+def udal():
+ conn = sqlite3.connect("pr.db")
+ cursor = conn.cursor()
+ sql = input('Введите ID объекта для удаления: ')
+ cursor.execute("DELETE FROM info WHERE id=?", sql)
+ conn.commit()
+ return
