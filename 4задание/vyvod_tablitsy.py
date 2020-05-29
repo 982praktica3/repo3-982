dc2d5817b47eaa2b13aeda5a5eed5495e68cf3f3
Ksen-Chek <65395448+Ksen-Chek@users.noreply.github.com>
2020-05-29 22:43:11 +0500

    Update vyvod_tablitsy.py

--- 4задание/vyvod_tablitsy.py
+++ 4задание/vyvod_tablitsy.py
@@ -1,17 +1,9 @@
 import sqlite3
-
-con = sqlite3.connect('pr.db')
-
-def sql_fetch(con):
-
-    cursorObj = con.cursor()
-
-    cursorObj.execute('SELECT * FROM info')
-
-    rows = cursorObj.fetchall()
-
-    for row in rows:
-
-        print(row)
-
-sql_fetch(con)
+def vyvod_tab():
+ con = sqlite3.connect('pr.db')
+ cursorObj = con.cursor()
+ cursorObj.execute('SELECT * FROM info')
+ rows = cursorObj.fetchall()
+ for row in rows:
+  print(row)
+ return
